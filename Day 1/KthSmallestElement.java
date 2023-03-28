489ce3cd9c4ecadb9e010c0ec87464f176688bf7
Rahul Anand Sahoo <101013741+rahulanand-08@users.noreply.github.com>
2023-03-29 00:12:20 +0530

    Update KthSmallestElement.java

--- Day 1/KthSmallestElement.java
+++ Day 1/KthSmallestElement.java
@@ -1,1 +1,22 @@
+//Link:-https://practice.geeksforgeeks.org/problems/kth-smallest-element5635/1
 
+
+class Solution{
+    public static int kthSmallest(int[] arr, int l, int r, int k) 
+    { 
+        PriorityQueue<Integer> pq=new PriorityQueue<>(Collections.reverseOrder());
+        for(int i=0;i<k;i++)
+        {
+            pq.add(arr[i]);
+        }
+        for (int i=k;i<arr.length;i++)
+        {
+            if (arr[i]<pq.peek())
+            {
+                pq.poll();
+                pq.add(arr[i]);
+            }
+        }
+        return pq.peek();
+    } 
+}
