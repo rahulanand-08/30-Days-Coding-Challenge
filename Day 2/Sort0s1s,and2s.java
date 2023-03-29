807abfab770e20bcd159b5cce20d57a9acee74ed
Rahul Anand Sahoo <101013741+rahulanand-08@users.noreply.github.com>
2023-03-29 22:06:41 +0530

    Update Sort0s1s,and2s.java

--- Day 2/Sort0s1s,and2s.java
+++ Day 2/Sort0s1s,and2s.java
@@ -1,1 +1,33 @@
+//Link:-https://practice.geeksforgeeks.org/problems/sort-an-array-of-0s-1s-and-2s4231/1
 
+class Solution
+{
+    public static void sort012(int a[], int n)
+    {
+        int start =0;
+        int mid=0;
+        int end=a.length-1;
+        while (mid<=end)
+        {
+            if(a[mid]==0)
+            {
+                swap(a,start,mid);
+                start++;
+                mid++;
+            }
+            else if (a[mid]==2)
+            {
+                swap(a,mid,end);
+                end--;
+            }
+            else 
+                mid++; 
+        }
+    }
+    public static void swap(int arr[],int i,int j)
+    {
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
+}
