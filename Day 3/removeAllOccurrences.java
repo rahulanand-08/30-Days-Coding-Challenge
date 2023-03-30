66259a240547c4c9ab78533dc8ec3fabdb72461f
Rahul Anand Sahoo <101013741+rahulanand-08@users.noreply.github.com>
2023-03-30 22:24:28 +0530

    Update removeAllOccurrences.java

--- Day 3/removeAllOccurrences.java
+++ Day 3/removeAllOccurrences.java
@@ -1,1 +1,12 @@
 
+//Link:- https://leetcode.com/problems/remove-all-occurrences-of-a-substring/
+
+class Solution {
+    public String removeOccurrences(String s, String part) {
+        while(s.contains(part)){
+            int i = s.indexOf(part);
+            s = s.substring(0,i)+s.substring(part.length()+i,s.length());
+        }
+        return s;
+    }
+}
