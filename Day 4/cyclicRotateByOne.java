b4cef50a44c6383fb718e29e66046f2668147cf7
Rahul Anand Sahoo <101013741+rahulanand-08@users.noreply.github.com>
2023-04-01 00:18:14 +0530

    Update cyclicRotateByOne.java

--- Day 4/cyclicRotateByOne.java
+++ Day 4/cyclicRotateByOne.java
@@ -1,1 +1,19 @@
+//Link:- https://practice.geeksforgeeks.org/problems/cyclically-rotate-an-array-by-one2614/1
 
+class Compute {
+    
+    public void rotate(int arr[], int n)
+    {
+        int temp[]=new int[n];
+        for (int i=0;i<n;i++)
+        {
+            temp[(i+1)%n]=arr[i];
+        }
+        int j=0;
+        for (int i:temp)
+        {
+            arr[j]=i;
+            j++;
+        }
+    }
+}
