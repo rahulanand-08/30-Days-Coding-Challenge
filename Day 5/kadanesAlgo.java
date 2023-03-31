d09f996810cb13970756888a51d9ea7e05a90e60
Rahul Anand Sahoo <101013741+rahulanand-08@users.noreply.github.com>
2023-04-01 00:30:15 +0530

    Update kadanesAlgo.java

--- Day 5/kadanesAlgo.java
+++ Day 5/kadanesAlgo.java
@@ -1,1 +1,22 @@
+//Link:-https://practice.geeksforgeeks.org/problems/kadanes-algorithm-1587115620/1
 
+class Solution{
+
+    // arr: input array
+    // n: size of array
+    //Function to find the sum of contiguous subarray with maximum sum.
+    long maxSubarraySum(int arr[], int n){
+        long sum=0;
+        long maxsum=Long.MIN_VALUE;
+        for (int i:arr)
+        {
+            sum=sum+i;
+            if (sum>maxsum)
+            maxsum=sum;
+            if (sum<0)
+            sum=0;
+        }
+        return maxsum;
+    }
+    
+}
