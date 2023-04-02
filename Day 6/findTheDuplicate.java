8befb91b16b95834005aee1e5617dd0739d66a75
Rahul Anand Sahoo <101013741+rahulanand-08@users.noreply.github.com>
2023-04-03 00:00:08 +0530

    Update findTheDuplicate.java

--- Day 6/findTheDuplicate.java
+++ Day 6/findTheDuplicate.java
@@ -1,1 +1,17 @@
+//Link:-https://leetcode.com/problems/find-the-duplicate-number/submissions/
 
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int freq[]=new int[nums.length+1];
+        for (int i:nums)
+        {
+            freq[i]++;
+        }
+        for (int i=0;i<freq.length;i++)
+        {
+            if (freq[i]>=2)
+                return i;
+        }
+        return -1;
+    }
+}
