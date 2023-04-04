c1c54c5084f5b2eb62b382d9c0087a9c3ff6e141
Rahul Anand Sahoo <101013741+rahulanand-08@users.noreply.github.com>
2023-04-04 23:03:10 +0530

    Update BuyAndSellStock.java

--- Day 8/BuyAndSellStock.java
+++ Day 8/BuyAndSellStock.java
@@ -1,1 +1,16 @@
+//Link:-https://leetcode.com/problems/best-time-to-buy-and-sell-stock/submissions/
 
+class Solution {
+    public int maxProfit(int[] prices) {
+        int buy = prices[0];
+        int profit = 0;
+        for(int i =1 ; i<prices.length ; i++){
+            if(buy< prices[i]){
+                profit = Math.max(prices[i] - buy , profit);
+            }else{
+                buy = prices[i];
+            }
+        }
+        return profit;
+    }
+}
