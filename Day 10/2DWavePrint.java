6dbfd2f79bff15ce8593b2e4ab8542e18b668e63
Rahul Anand Sahoo <101013741+rahulanand-08@users.noreply.github.com>
2023-04-06 23:12:16 +0530

    Update 2DWavePrint.java

--- Day 10/2DWavePrint.java
+++ Day 10/2DWavePrint.java
@@ -1,1 +1,26 @@
+//Link:-https://www.codingninjas.com/codestudio/problems/print-like-a-wave_893268
 
+public class Solution {
+	public static int[] wavePrint(int arr[][], int nRows, int mCols) {
+		ArrayList<Integer> ans = new ArrayList<Integer>();
+		for (int col = 0; col < mCols; col++)
+		{
+			if (col % 2 != 0) //(col & 1) != 0
+			{
+				for (int row = nRows - 1; row >= 0; row--)
+				{
+					ans.add(arr[row][col]);
+				}
+			}
+				else
+				{
+					for (int row = 0; row < nRows; row++)
+					{
+						ans.add(arr[row][col]);
+					}
+				}
+		}
+		int[] arr1 = ans.stream().mapToInt(i -> i).toArray();
+		return arr1;
+	}
+}
