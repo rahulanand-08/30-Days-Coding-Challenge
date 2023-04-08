51ea947517afae973276e1b205be2f14abea867e
Rahul Anand Sahoo <101013741+rahulanand-08@users.noreply.github.com>
2023-04-08 23:17:54 +0530

    Update CountPrime.java

--- Day 12/CountPrime.java
+++ Day 12/CountPrime.java
@@ -1,1 +1,21 @@
+//Link:-https://leetcode.com/problems/count-primes/submissions/
 
+class Solution {
+    public int countPrimes(int n) {
+    boolean a[]=new boolean [n];
+    long c=0;
+    for(long i=2;i<n;i++)
+    {
+        if(a[(int)i])
+        continue;
+        c++;
+        for(long j=i*i;j<n;j+=i)
+        {
+            a[(int)j]=true;
+        }
+
+    }
+    return (int)c;
+        
+    }
+}
