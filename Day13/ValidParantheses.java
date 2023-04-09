de8ccd1055b919c1de24e799eb0085667516f8e9
Rahul Anand Sahoo <101013741+rahulanand-08@users.noreply.github.com>
2023-04-09 23:34:55 +0530

    Update ValidParantheses.java

--- Day13/ValidParantheses.java
+++ Day13/ValidParantheses.java
@@ -1,1 +1,26 @@
+import java.util.*;
+class Solution {
+    public boolean isValid(String s) 
+    {
+       Stack<Character> st=new Stack<>();
+       for(int i=0;i<s.length();i++)
+       {
+           char c=s.charAt(i);
+           if((c=='(') || (c=='{') || (c=='['))
+           st.push(c);
 
+           else if(st.isEmpty())
+           return false;
+
+           else if( (st.peek()=='(' && c==')') ||
+                    (st.peek()=='{' && c=='}') ||
+                    (st.peek()=='[' && c==']') )
+                    st.pop();
+
+                    else 
+                    return false;
+
+       }
+       return st.empty();
+    }
+}
