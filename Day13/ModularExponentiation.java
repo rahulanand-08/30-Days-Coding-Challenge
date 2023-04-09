0fa50225982529c9e54f59f369e7fbbf9c5be61f
Rahul Anand Sahoo <101013741+rahulanand-08@users.noreply.github.com>
2023-04-09 23:29:36 +0530

    Update ModularExponentiation.java

--- Day13/ModularExponentiation.java
+++ Day13/ModularExponentiation.java
@@ -1,1 +1,25 @@
+import java.io.*;
+import java.util.* ;
 
+public class Solution {
+    public static int modularExponentiation(int a, int y, int p) 
+    {
+           long res = 1;
+           long x=a;
+           x=x%p;
+
+           if(x==0)
+           return 0;
+ 
+        while (y > 0) 
+        {
+            if ((y & 1) != 0)
+                res = (res*x%p);
+ 
+           
+            y = y >> 1;
+            x = ((x * x)%p); 
+        }
+        return (int)res ;   
+    }
+}
