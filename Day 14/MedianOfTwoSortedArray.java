ac5c0127678bbfc8e7e98adeb3b23da52362e82b
Rahul Anand Sahoo <101013741+rahulanand-08@users.noreply.github.com>
2023-04-10 22:04:57 +0530

    Update MedianOfTwoSortedArray.java

--- Day 14/MedianOfTwoSortedArray.java
+++ Day 14/MedianOfTwoSortedArray.java
@@ -1,1 +1,29 @@
 //Link:-https://leetcode.com/problems/median-of-two-sorted-arrays/
+
+import java.util.*;
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int n=nums1.length;
+        int m=nums2.length;
+        int main[]=new int [n+m];
+        int c=0;
+        for (int i=0;i<n;i++)
+        {
+            main[c]=nums1[i];
+            c++;
+        }
+        for (int i=0;i<m;i++)
+        {
+            main[c]=nums2[i];
+            c++;
+        }
+        Arrays.sort(main);
+        int mid=main.length/2;
+        if (main.length%2==0)
+        {
+            return (double)(main[mid-1] + main[mid])/2;
+        }
+        else
+            return main[mid];
+    }
+}
