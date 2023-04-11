72d0bfd19cb5b03a690423a78a31623b8e2b7478
Rahul Anand Sahoo <101013741+rahulanand-08@users.noreply.github.com>
2023-04-11 23:38:40 +0530

    Update ModularExponential.java

--- Day 15/ModularExponential.java
+++ Day 15/ModularExponential.java
@@ -1,1 +1,16 @@
+//Link:-https://www.codingninjas.com/codestudio/problems/modular-exponentiation_1082146?leftPanelTab=1
 
+public class Solution {
+    public static int modularExponentiation(int x, int n, int m) {
+        long ans = 1;
+        long xx = x;
+        while(n>0){
+             if(n%2 !=0){
+                ans = ((ans)%m *(xx)% m ) %m;
+            }
+            xx = ((xx)%m *(xx)%m)%m;
+            n= n>>1;
+        }  
+        return(int) (ans%m);      
+    }
+}
