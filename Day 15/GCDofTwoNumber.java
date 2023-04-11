3c56c08f48f7ff865f4c922cbe23149c428de383
Rahul Anand Sahoo <101013741+rahulanand-08@users.noreply.github.com>
2023-04-11 22:40:08 +0530

    Update GCDofTwoNumber.java

--- Day 15/GCDofTwoNumber.java
+++ Day 15/GCDofTwoNumber.java
@@ -1,1 +1,24 @@
+//Link:-https://leetcode.com/problems/find-greatest-common-divisor-of-array/submissions/
 
+class Solution {
+    public int findGCD(int[] nums) {
+        int min = nums[0], max = nums[0];
+
+        for(int i=1; i<nums.length; i++)
+        {
+            if(max < nums[i]) max = nums[i];
+            else if(min > nums[i]) min = nums[i];
+        }
+        
+        return getGCD(max, min);
+    }
+
+    public int getGCD(int a, int b)
+    {
+        if(a==b) return a;
+        
+        if(a>b) return getGCD(a-b, a);
+        return getGCD(a, b-a);
+    }
+    
+}
