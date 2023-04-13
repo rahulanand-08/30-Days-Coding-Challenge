0dd74ac42e04ee6df07637ef0515cdf695b1c182
Rahul Anand Sahoo <101013741+rahulanand-08@users.noreply.github.com>
2023-04-13 21:55:38 +0530

    Update IsSortedRecursion.java

--- Day 17/IsSortedRecursion.java
+++ Day 17/IsSortedRecursion.java
@@ -1,1 +1,12 @@
+//Link:-https://practice.geeksforgeeks.org/problems/check-if-an-array-is-sorted0701/1
 
+
+class Solution {
+    boolean arraySortedOrNot(int[] arr, int n) {
+        if (n == 1 || n == 0)
+            return true;
+        if (arr[n-1] < arr[n-2])
+            return false;
+        return arraySortedOrNot(arr,n-1);
+    }
+}
