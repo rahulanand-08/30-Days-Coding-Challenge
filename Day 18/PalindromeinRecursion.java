1a6d758f3945c6c76ca028c7415400997d6282ea
Rahul Anand Sahoo <101013741+rahulanand-08@users.noreply.github.com>
2023-04-14 00:07:35 +0530

    Update PalindromeinRecursion.java

--- Day 18/PalindromeinRecursion.java
+++ Day 18/PalindromeinRecursion.java
@@ -1,1 +1,20 @@
 
+import java.util.*;
+public class solution {
+	static boolean palindrome(String str, int i, int j)
+	{
+		if (i>j)
+			return true	;
+		 if (str.charAt(i)!=str.charAt(j))
+			return false;
+		else 
+			return palindrome(str,i+1,j-1);
+	}
+	
+	public static void main(String[] args) 
+	
+	{
+		boolean palin=palindrome("nitin",0, 4);
+        System.out.println(palin);
+	}
+}
