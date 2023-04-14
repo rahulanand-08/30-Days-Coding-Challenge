6ec5ad130d896e717c728da5786d148bdbc04769
Rahul Anand Sahoo <101013741+rahulanand-08@users.noreply.github.com>
2023-04-14 23:26:14 +0530

    Update sumOfElementsUsingRecursion.java

--- Day 18/sumOfElementsUsingRecursion.java
+++ Day 18/sumOfElementsUsingRecursion.java
@@ -1,1 +1,21 @@
 
+import java.util.*;
+public class solution {
+	static int sum=0;
+	static int arrsum(int arr[], int n)
+	{
+		if (n==0)
+			return 0;
+		else 
+			sum=sum+arr[n-1];
+		 arrsum(arr,n-1);
+		 return sum;
+	}
+	
+	public static void main(String[] args) 
+	
+	{
+		int arr[]= {2,3,4,5};
+        System.out.println(arrsum(arr,4));
+	}
+}
