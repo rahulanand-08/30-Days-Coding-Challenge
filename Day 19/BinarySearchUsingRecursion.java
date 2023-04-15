3c86e1264dd0bba5e96f1d9bea5696013650ca9e
Rahul Anand Sahoo <101013741+rahulanand-08@users.noreply.github.com>
2023-04-15 23:06:59 +0530

    Update BinarySearchUsingRecursion.java

--- Day 19/BinarySearchUsingRecursion.java
+++ Day 19/BinarySearchUsingRecursion.java
@@ -1,1 +1,28 @@
+public class BinarySearchUsingRecursion 
+{
+	
+	public static int binarysearch(int a[],int s,int e,int key)
+	{
+		int mid=s+(e-s)/2;
+		
+		if(a[mid]==key)
+			return mid;
+		if(s>e)
+			return -1;
+		
+		
+		 if(a[mid]<key)
+			return binarysearch(a,mid+1,e,key);
+		else
+			return binarysearch(a,s,mid-1,key);
 
+	}
+
+	public static void main(String[] args) 
+	{
+		int a[]= {2,4,5,7,9};
+		System.out.println(binarysearch(a,0,a.length-1,5));
+
+	}
+
+}
